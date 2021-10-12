74d687a419c90d6d10a3b04b6371279286e1bcd4
Shengyou Fan <shengyoufan@gmail.com>
2021-10-12 22:18:50 +0800

    add Android UI

--- androidApp/src/main/java/io/kraftsman/app/kmmkungfu/android/MainActivity.kt
+++ androidApp/src/main/java/io/kraftsman/app/kmmkungfu/android/MainActivity.kt
@@ -2,15 +2,24 @@ package io.kraftsman.app.kmmkungfu.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import io.kraftsman.app.kmmkungfu.Detector
 import android.widget.TextView
+import io.kraftsman.app.kmmkungfu.PasswordGenerator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = Detector().detect()
+        val passwordTextView: TextView = findViewById(R.id.responseText)
+        val versionTextView: TextView = findViewById(R.id.versionTextView)
+
+        versionTextView.text = Detector().detect()
+
+        val generateButton: Button = findViewById(R.id.generateBtn)
+        generateButton.setOnClickListener {
+            passwordTextView.text = PasswordGenerator().generate(7)
+        }
     }
 }
