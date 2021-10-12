40bea8cb9441d6f702b3c360a973ca86f2d3a993
Shengyou Fan <shengyoufan@gmail.com>
2021-10-12 22:50:03 +0800

    add coroutine dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,14 +8,16 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
 }
 
 android {
-    compileSdkVersion(31)
+    compileSdk = 31
     defaultConfig {
         applicationId = "io.kraftsman.app.kmmkungfu.android"
-        minSdkVersion(30)
-        targetSdkVersion(31)
+        minSdk = 30
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
     }
